38a80080cb36a57df8c488838ba7055e07d7f56f
Your Name <you@example.com>
2022-12-25 00:02:26 +0100

    sort impl

---
+++ structures/src/main/java/dev/vvasiliev/structures/sort/MergeSort.kt
@@ -0,0 +1,65 @@
+package dev.vvasiliev.structures.sort
+
+class MergeSort {
+
+    val arrayExample = Array(4500000) { index ->
+        (Math.random() * if (index % 2 > 0) index * 10 else -index * 10).toInt()
+    }
+
+    fun Array<Int>.split(): List<Array<Int>> {
+        if (size > 1) {
+            val middle = if (size % 2 > 0) (size / 2) + 1 else (size / 2)
+            return listOf(copyOfRange(0, middle), copyOfRange(middle, size))
+        } else throw Exception("can not split anymore")
+    }
+
+    fun Array<Int>.print() {
+        print("[")
+        this.forEachIndexed { index, i ->
+            print("$i ")
+        }
+        print("] ")
+    }
+
+    infix fun Array<Int>.merge(array: Array<Int>): Array<Int> {
+        return arrayOf(*this, *array).apply { sort() }
+    }
+
+    val splittedArrays = mutableListOf<Array<Int>>()
+    var mergedArrays = mutableListOf<Array<Int>>()
+
+    fun sort(array: Array<Int>): MutableList<Array<Int>> {
+        mergeSort(*splittedArrays.toTypedArray())
+        return mergedArrays
+    }
+
+    fun recursiveSort(array: Array<Int>) {
+        if (array.size > 1) {
+            array.split().iterator().run {
+                while (hasNext())
+                        recursiveSort(this@run.next())
+            }
+        } else {
+            splittedArrays.add(array)
+        }
+    }
+
+    private fun mergeSort(vararg arrays: Array<Int>) {
+        if (arrays.size > 1) {
+            val newArray = mutableListOf<Array<Int>>()
+            val iterator = arrays.iterator()
+            while (iterator.hasNext()) {
+                newArray.add(iterator.run {
+                    val left = next()
+                    if (hasNext()) {
+                        val right = next()
+                        left merge right
+                    } else left
+                })
+            }
+            mergeSort(*newArray.toTypedArray())
+        } else {
+            mergedArrays = arrays.toMutableList()
+        }
+    }
+}
--- structures/src/test/java/dev/vvasiliev/structures/ExampleUnitTest.kt
+++ structures/src/test/java/dev/vvasiliev/structures/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package dev.vvasiliev.structures
 
+import dev.vvasiliev.structures.sort.MergeSort
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +16,21 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    @OptIn(ExperimentalTime::class)
     @Test
     fun addition_isCorrect() {
+
+        val merger = MergeSort()
+        merger.run {
+            recursiveSort(arrayExample)
+            val duration = measureTime {
+                    sort(arrayExample).toTypedArray()[0]
+            }
+            val duration2 = measureTime {
+                arrayExample.sort()
+            }
+            print("\n Time: ${duration.inWholeSeconds} seconds; Time2: ${duration2.inWholeSeconds}\n")
+        }
         assertEquals(4, 2 + 2)
     }
 }
